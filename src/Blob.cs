fabb87cf661205cc16ffb4533b0e707575913a7a
Aiden Bradley <116838707+AidenBradley24@users.noreply.github.com>
2024-06-23 16:14:52 -0400

    read blob :)

---
+++ src/Blob.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Compression;
+
+namespace codecrafters_git.src
+{
+    internal class Blob(string hash)
+    {
+        private readonly string hash = hash;
+
+        public byte[] Read()
+        {
+            FileInfo info = new(Path.Combine(".git", "objects", hash[..2], hash[2..]));
+            using var fs = info.Open(FileMode.Open);
+            using var zl = new ZLibStream(fs, CompressionMode.Decompress);
+            zl.Seek("blob ".Length, SeekOrigin.Begin);
+            using var br = new BinaryReader(zl);
+            List<byte> bees = [];
+            while (true)
+            {
+                byte b = br.ReadByte();
+                if (b == '\0')
+                {
+                    break;
+                }
+                bees.Add(b);
+            }
+
+            int length = int.Parse(bees.ToArray());
+            byte[] content = br.ReadBytes(length);
+            return content;
+        }
+    }
+}
--- src/Program.cs
+++ src/Program.cs
@@ -1,3 +1,4 @@
+using codecrafters_git.src;
 using System;
 using System.IO;
 
@@ -5,6 +6,15 @@ if (args.Length < 1)
 {
     Console.WriteLine("Please provide a command.");
     return;
+}
+
+string? plumb = null;
+for (int i = 0; i < args.Length; i++)
+{
+    if (args[i] == "--p")
+    {
+        plumb = args[++i];
+    }
 }
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -19,6 +29,11 @@ if (command == "init")
     Directory.CreateDirectory(".git/refs");
     File.WriteAllText(".git/HEAD", "ref: refs/heads/main\n");
     Console.WriteLine("Initialized git directory");
+}
+else if (command == "cat-file")
+{
+    var blob = new Blob(plumb!);
+    Console.WriteLine(blob.Read());
 }
 else
 {
