2fe471148da62827e4d0e18c860dabae4f5e6d10
Aiden Bradley <116838707+AidenBradley24@users.noreply.github.com>
2024-06-23 18:52:38 -0400

    test

--- src/Blob.cs
+++ src/Blob.cs
@@ -42,8 +42,8 @@ namespace codecrafters_git.src
                 Target.Directory!.Create();
             }
             using var fs = Target.OpenWrite();
-            fs.Write(Encoding.ASCII.GetBytes($"blob {source.Length}\0"));
             using var zl = new ZLibStream(fs, CompressionMode.Compress);
+            zl.Write(Encoding.ASCII.GetBytes($"blob {source.Length}\0"));
             source.CopyTo(zl);
         }
 
