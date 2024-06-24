fa2479237e52c4447044c33293a45eccef2b35fb
Aiden Bradley <116838707+AidenBradley24@users.noreply.github.com>
2024-06-24 14:11:19 -0400

    test

--- src/Tree.cs
+++ src/Tree.cs
@@ -20,7 +20,7 @@ namespace codecrafters_git.src
             foreach (var dir in sourceDir.EnumerateDirectories())
             {
                 Tree child = Create(dir);
-                entries.Add(new(child.HashBytes, sourceDir.Name, "040000"));
+                entries.Add(new(child.HashBytes, sourceDir.Name, "40000"));
             }
 
             foreach (var file in sourceDir.EnumerateFiles())
